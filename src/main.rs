e5fcb949630b5b68b514f6fcb5891a276f855708
Lewis3579 <117494220+Lewis3579@users.noreply.github.com>
2023-12-28 10:10:14 +0700

    no message

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
 #![no_std]
 #![no_main]
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-static HELLO: &[u8] = b"Heeeeeelllloo!";
-#[no_mangle] // don't mangle the name of this function
+use blog_os::println;
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("100% copy{}", "!");
+    println!("Hello World{}", "!");
 
+    blog_os::init();
+
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+    // trigger a stack overflow
+    stack_overflow();
+
+    // as before
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
     loop {}
-
-
 }
 
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
